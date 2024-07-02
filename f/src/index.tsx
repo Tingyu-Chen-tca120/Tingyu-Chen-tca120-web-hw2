27185490bd4012529f9854d6c8146341cddef68f
Tingyu-Chen-tca120 <84502134+Tingyu-Chen-tca120@users.noreply.github.com>
2024-07-02 09:52:38 +0800

    Add files via upload

---
+++ f/src/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { ErrorPage } from './components/pages/error/error';
+import { Dashboard } from './components/pages/dashboard/dahsboard';
+import { Login } from './components/pages/login/login';
+import { UserProvider } from './components/common/user-context/user-context';
+import { Register } from './components/pages/login/register';
+import RoutesSearch from './components/common/RoutesSearch';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+// Define the routes for the application and components to display
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Dashboard />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/register',
+    element: <Register />,
+    errorElement: <ErrorPage />
+  }
+]);
+
+// Render the application with routes
+root.render(
+  <React.StrictMode>
+    <UserProvider>
+      <RouterProvider router={router} />
+    </UserProvider>
+  </React.StrictMode>
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RoutesSearch />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ f/src/util/util.ts
@@ -0,0 +1,31 @@
+import { type SelectChangeEvent } from '@mui/material';
+
+/**
+ * Url to the main api endpoint
+ * @returns string for the api endpoint
+ */
+export function getApiUrl() {
+  return process.env.REACT_APP_API_ENDPOINT ?? 'http://localhost:8090';
+}
+
+/**
+ * A generic function for calling a set state function from a mui input
+ * @param setFn the function to call with the value when changed
+ * @returns on change function that sets the state
+ */
+export function getHandleChange(setFn: (val: string) => void) {
+  return (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | SelectChangeEvent) => {
+    setFn(event.target.value);
+  }
+}
+
+/**
+ * A generic function for calling a set state function from mui checkbox
+ * @param setFn the function to call with the value when changed
+ * @returns on change function that sets the state
+ */
+export function getCheckboxChange(setFn: (val: boolean) => void) {
+  return (event: React.SyntheticEvent<Element, Event>, checked: boolean) => {
+    setFn(checked);
+  }
+};
